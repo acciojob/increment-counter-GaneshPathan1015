f75d551bb3823d70f07b12d640b9ce948569f13e
Ganesh Pathan <118661019+GaneshPathan1015@users.noreply.github.com>
2023-12-26 15:17:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 //your JS code here. If required.
-const para = document.getElementById("counter");
-let counter = 0;
-const incbtn = document.getElementById("incrementBtn");
-incbtn.addEventListener("click", ()=>{
-	alert(`${counter}`);
-	counter++;
-	para.innerText=counter;
-})
+// const para = document.getElementById("counter");
+// let counter = 0;
+// const incbtn = document.getElementById("incrementBtn");
+// incbtn.addEventListener("click", ()=>{
+// 	counter++;
+// 	alert(`${counter}`);
+// 	para.innerText=counter;
+// })
+
+function incrementCounter() {
+  const counterElement = document.getElementById('counter');
+  const currentValue = parseInt(counterElement.innerText, 10);
+  alert(`Current value: ${currentValue}`);
+  const incrementedValue = currentValue + 1;
+  counterElement.innerText = incrementedValue;
+}
